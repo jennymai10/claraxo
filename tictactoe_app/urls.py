de26a5da28af7cb888623cb8c23084cfb1e29a6f
Jenny Mai <nhumai2@illinois.edu>
2024-08-31 13:24:12 +1000

    update login function

--- tictactoe_app/urls.py
+++ tictactoe_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 from .views import handle_login
 
 urlpatterns = [
+    path('', handle_login, name='login'),
     path('register/', register_user, name='register_user'),
     path('users/', views.get_users, name='get_users'),
     path('login/', handle_login, name='login'),
