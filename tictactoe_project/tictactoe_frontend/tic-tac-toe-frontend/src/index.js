48f5ff5a813cdfbb6523dcfdc2412f3abfedb356
Jenny Mai <nhumai2@illinois.edu>
2024-09-15 23:09:28 +1000

    Modify CSRF Verification in react + relocate a few files

--- tictactoe_project/tictactoe_frontend/tic-tac-toe-frontend/src/index.js
+++ tictactoe_project/tictactoe_frontend/tic-tac-toe-frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Playboard from './playboard'; // 引入Playboard组件
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // 引入react-router-dom中的组件
+import Login from './Login';
+import Playboard from './playboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Login />} />
         <Route path="/playboard" element={<Playboard />} />
       </Routes>
     </Router>
