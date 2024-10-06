206bf515b14915e136b39d230abd4d686196d6ae
jennymmai <ntmai@student.unimelb.edu.au>
2024-10-07 01:30:32 +1100

    Refactor CORS and CSRF settings to allow specific origins and ports

--- tictactoe_project/tictactoe_backend/settings.py
+++ tictactoe_project/tictactoe_backend/settings.py
@@ -127,12 +127,12 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'clara.tictactoe@gmail.com'
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://35.238.92.0',
+    'http://35.238.92.0:3000',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
-    'http://35.238.92.0',
+    'http://35.238.92.0:3000',
 ]
