fbc24a54e87de62845aa0d10774e3a0820ec4e74
jennymmai <ntmai@student.unimelb.edu.au>
2024-10-21 20:30:21 +1100

    Refactor manage.py to remove unnecessary code

--- manage.py
+++ manage.py
@@ -2,11 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
-
 
 def main():
-    load_dotenv()
 
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clara_project.settings')
