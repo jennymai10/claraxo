621162422532b1eca224178cbfd9fb8428fa95fc
jennymmai <ntmai@student.unimelb.edu.au>
2024-10-26 20:20:55 +1100

    Refactor CI workflow to update SECRET_KEY environment variable value

--- tictactoe_project/tictactoe_backend/settings.py
+++ tictactoe_project/tictactoe_backend/settings.py
@@ -8,8 +8,7 @@ load_dotenv(dotenv_path='./.env.production')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
